343053c87283370b3a1e0e904278728bd18a0f9b
Jeff Doss <notdsk@gmail.com>
2018-10-16 08:19:41 -0400

    origin-py-setup: WIP setup classifiers
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- python-project/setup/embryo.py
+++ python-project/setup/embryo.py
@@ -1,6 +1,10 @@
+from copy import deepcopy
+
 from appyratus.validation import fields
 from appyratus.io import Ini
 from embryo import Embryo
+
+from . import constants    # import PROJECT_CLASSIFIERS
 
 
 class SetupEmbryo(Embryo):
@@ -17,8 +21,17 @@ class SetupEmbryo(Embryo):
         description = fields.Str(allow_none=True, default='')
         version = fields.Anything(allow_none=True, default='0b0')
         tagline = fields.Str(allow_none=True)
+        author = fields.Str(allow_none=True)
+        author_email = fields.Str(allow_none=True)
+        url = fields.Str(allow_none=True)
+        classifiers = fields.List(fields.Str(), default=[])
 
     def on_create(self, context):
-        config_data = self.fs['/setup.cfg']
-        import ipdb; ipdb.set_trace(); print('=' * 100)
+        setup_config = self.fs['/setup.cfg']
+        # clone the existing config
+        setup_context = deepcopy(context)
+        del setup_context['embryo']
+        import ipdb
+        ipdb.set_trace()
+        print('=' * 100)
         # XXX merge context

705192a6e09ea74cae0b167bca093d6db21e770b
Jeff Doss <notdsk@gmail.com>
2018-11-19 08:27:32 -0500

    schema.v2: Working on docker stuffs
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

---
+++ docker-project/embryo.py
@@ -0,0 +1,15 @@
+from appyratus.schema import fields
+from embryo import Embryo
+
+
+class DockerProjectEmbryo(Embryo):
+    """
+    # Docker Project Embryo
+    """
+
+    class context_schema(Embryo.Schema):
+        """
+        # Context Schema
+        The respective Docker Project schema
+        """
+        pass

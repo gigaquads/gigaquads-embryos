d48ba5337640817da9881cf3d237f5ae099064c9
Jeff Doss <notdsk@gmail.com>
2018-12-06 12:00:20 -0500

    schema.v2: Pybiz env data path
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- pybiz-project/env/templates/env.py
+++ pybiz-project/env/templates/env.py
@@ -2,8 +2,14 @@ from appyratus.env import Environment
 from appyratus.schema import fields
 
 class {{ project.context.project.name|camel }}Environment(Environment):
+    {{ project.context.project.name|snake|upper }}_DATA_PATH = fields.FilePath(required=True)
     {{ project.context.project.name|snake|upper }}_MANIFEST_FILEPATH = fields.FilePath(required=True)
+
+    @property
+    def data_path(self):
+        return self.{{ project.context.project.name|snake|upper }}_DATA_PATH
 
     @property
     def manifest_filepath(self):
         return self.{{ project.context.project.name|snake|upper }}_MANIFEST_FILEPATH
+
